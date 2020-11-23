1615193d8cf4affd6ed78f869fb690100f97d8ca
Honshu11 <egonza67@yahoo.com>
2020-11-22 19:10:18 -0800

    Update rps.js
    made fix for userChoice to target HTML data atribute (choice)

--- rps.js
+++ rps.js
@@ -1,13 +1,13 @@
-// selectors for corresponding buttons, rock, paper, scissors and play buttons.
+// selectors for corresponding button rock paper scissors class.
 
 const gameButtons = document.querySelectorAll('.rpsButtons button'); // querySelectorAll selects whole nodes(arrays)
-const displayResult = document.querySelector('.display-result h3'); //querySelector selects individual elements
+const displayResult = document.querySelector('.display-result h3'); //querySelector selects individual elements - html h3
 const compResult = document.querySelector('.display-result p');
 
 // listeners
 
-for(let i = 0; i < gameButtons.length; i++){
-    gameButtons[i].addEventListener('click', userChoice);  
+for(let i = 0; i < gameButtons.length; i++){  //iterating through gameButtons(which is an array using querySelectorAll)
+    gameButtons[i].addEventListener('click', userChoice); 
 }
 
 //global variable for rock paper scissors game. string array
@@ -19,14 +19,14 @@ const options = ['rock', 'paper', 'scissors'];
 function userChoice(event){
     const choice = event.target.dataset.choice;  // using event.target but specifically targetting the choice of the user.
     //console.log(choice);  // displays the data of the variable
-    const compChoice = cpuChoice(); // 
+    const compChoice = cpuChoice();
     const result = determineWin(choice, compChoice); 
     displayResult.textContent = result; // using selector displayResult to display the result on html
     compResult.textContent = "cpu choose: " + compChoice; // concatinating the compResult and displaying using HTML textContent property.
 }
 
 
-function cpuChoice(){    // creating function for selecting computers choice 
+function cpuChoice(){  // creating function for selecting computers choice 
     const randomIndex = Math.floor(Math.random() * options.length); // get the global variable options and use the length property to select cpu pick
     return options[randomIndex];  // returns whatever the randomIndex option was selected. rock , paper, scissor.
 }
@@ -44,10 +44,10 @@ function determineWin(user, comp){
     let a = getIndex(user);
     let b = getIndex(comp);
     if(user === comp){
-        return 'draw';
+        return 'Draw';
     }
 
-    if((a - b + 3) % 3 == 1) {  //internet ftw!
+    if((a - b + 3) % 3 == 1) {  //internet ftw
         return 'You win!';
     } else {
         return 'You lose';
